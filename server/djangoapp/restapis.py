a1ef6f16be9542ce86161cec971390d07342a8f4
MaithaBin <141325017+MaithaBin@users.noreply.github.com>
2024-06-30 15:32:26 +0900

    Fixed linting errors

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -22,9 +22,9 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except User.DoesNotExist:
+    except Exception as e:
         # If any error occurs
-        print("Network exception occurred")
+        print(f"Error: {e}")
 
 
 def analyze_review_sentiments(text):
@@ -35,7 +35,6 @@ def analyze_review_sentiments(text):
         return response.json()
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
-        print("Network exception occurred")
 
 
 def post_review(data_dict):
@@ -44,5 +43,5 @@ def post_review(data_dict):
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except User.DoesNotExist:
-        print("Network exception occurred")
+    except Exception as e:
+        print(f"Error: {e}")
